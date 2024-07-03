05f36867010607f7d8af6a17798bf8fb9cc2aa64
Achmad Fatah Ibrahim <acfatah@gmail.com>
2024-07-03 19:50:58 +0800

    The script to generate the data

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+  {
+    formatters: {
+      css: true,
+      html: true,
+      markdown: 'prettier',
+    },
+
+    rules: {
+      'no-console': 'off',
+    },
+  },
+)
